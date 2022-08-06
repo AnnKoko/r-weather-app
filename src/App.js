d2150a35abfdf3bd372b13d2d18ad1e891d90bc5
AnnKoko <49923591+AnnKoko@users.noreply.github.com>
2022-08-06 15:36:57 -0400

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 
 function App() {
-  return <div className="App">
-  <h1>Weather App<h1>
+  return(
+<div className="App">
+  <h1>Weather App</h1>
   <footer>
     This project was coded by Anna Kokozei and is {""}
     <a href="https://github.com/AnnKoko/r-weather-app" target="_blank">
-    open-sourced on Github </a> 
+    open-sourced on Github </a>
   </footer>
-  </div>;
+  </div>
+);
 }
 
 export default App;
